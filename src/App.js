9a4dd385cc43fb2d578cefc6140f4fa1eef394dc
origami unicorn <rvwright@gmail.com>
2020-02-28 17:22:20 -0800

    Added bootstrap-react and worked on App.js, added Jumbotron, imported Jumbotron, Container, Nav, and Navbar, added backgroundImage to jumbotron.

--- src/App.js
+++ src/App.js
@@ -1,25 +1,34 @@
 import React from 'react';
-import logo from './logo.svg';
+import Jumbotron from 'react-bootstrap/Jumbotron';
+import Container from 'react-bootstrap/Container';
+import Navbar from 'react-bootstrap/Navbar';
+import Nav from 'react-bootstrap/Nav';
 import './App.css';
+
+const jumboStyle = {
+  backgroundImage: 'url("https://images.unsplash.com/photo-1507103011901-e954d6ec0988?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1350&q=80")',
+  backgroundPosition: 'center',
+  backgroundSize: 'cover'
+}
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <Navbar fixed="top">
+        <Navbar.Brand href="#home">Navbar</Navbar.Brand>
+        <Nav className="mr-auto">
+          <Nav.Link href="#home">Home</Nav.Link>
+          <Nav.Link href="#features">Features</Nav.Link>
+          <Nav.Link href="#pricing">Pricing</Nav.Link>
+        </Nav>
+      </Navbar>
+      <Jumbotron fluid style={jumboStyle}>
+        <Container>
+          <h1>Fluid Jumbotron</h1>
+          <p>Modified to hold the horizontal space.</p>
+        </Container>
+      </Jumbotron>
+    </>
   );
 }
 
