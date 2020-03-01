988cd69c3df9dcc6801f8953850dddceb8665df2
origami unicorn <rvwright@gmail.com>
2020-02-29 22:22:22 -0800

    Importing images, adding to image array, checking scores onClick in order to appropriately update and then reshuffle images.

--- src/components/pages/Click.js
+++ src/components/pages/Click.js
@@ -7,10 +7,24 @@ import Button from 'react-bootstrap/Button';
 import Alert from 'react-bootstrap/Alert';
 import Card from 'react-bootstrap/Card';
 import CardColumns from 'react-bootstrap/CardColumns';
+import Img01 from '../../assets/images/01.png';
+import Img02 from '../../assets/images/02.png';
+import Img03 from '../../assets/images/03.png';
+import Img04 from '../../assets/images/04.png';
+import Img05 from '../../assets/images/05.png';
+import Img06 from '../../assets/images/06.png';
+import Img07 from '../../assets/images/07.png';
+import Img08 from '../../assets/images/08.png';
+import Img09 from '../../assets/images/09.png';
+import Img10 from '../../assets/images/10.png';
+import Img11 from '../../assets/images/11.png';
+import Img12 from '../../assets/images/12.png';
+import Background from '../../assets/images/background.jpg';
+
 
 const jumboStyle = {
     color: "#ffffff",
-    backgroundImage: 'url("https://images.unsplash.com/photo-1507103011901-e954d6ec0988?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1350&q=80")',
+    backgroundImage: `url(${Background})`,
     backgroundPosition: 'center',
     backgroundSize: 'cover'
 }
@@ -21,24 +35,64 @@ const cardColOverride = {
 
 let imgArray = [
     {
-        image: "https://www.fillmurray.com/100/100",
-        alt: "Bill Murray.",
+        image: Img01,
+        alt: "Snowy mountains beyond a fall meadow with an elevated wooden path through it.",
         name: 1
     },
     {
-        image: "https://picsum.photos/seed/picsum/100/100",
-        alt: "Seeded Image.",
+        image: Img02,
+        alt: "Sheer rock mountain face rising above coniferous tree level.",
         name: 2
     },
     {
-        image: "https://picsum.photos/100/100/?blur",
-        alt: "Blurry Photo.",
+        image: Img03,
+        alt: "Looking out over a valley between sheer red rock cliffs with green along the valley river.",
         name: 3
     },
     {
-        image: "https://picsum.photos/100/100?grayscale",
-        alt: "Grayscale.",
+        image: Img04,
+        alt: "Fog disguising sharp mountain peaks with rock faces and plants growing on steep inclines.",
         name: 4
+    },
+    {
+        image: Img05,
+        alt: "Mountains rising above bright blue lake.",
+        name: 5
+    },
+    {
+        image: Img06,
+        alt: "Sunset over snowy mountain peaks.",
+        name: 6
+    },
+    {
+        image: Img07,
+        alt: "Snow covered mountains reflected in a lake.",
+        name: 7
+    },
+    {
+        image: Img08,
+        alt: "Sheer rock cliffs at a beach.",
+        name: 8
+    },
+    {
+        image: Img09,
+        alt: "Desert landscape with rock formations in the distance at sunset.",
+        name: 9
+    },
+    {
+        image: Img10,
+        alt: "Blunted mountains above a winding river with snowy peaks in the distance.",
+        name: 10
+    },
+    {
+        image: Img11,
+        alt: "Gently rising mountains covered in pathways with red and green flora.",
+        name: 11
+    },
+    {
+        image: Img12,
+        alt: "Looking down on Machu Picchu in the Andes on a sunny day.",
+        name: 12
     }
 ];
 
@@ -118,63 +172,78 @@ class Click extends React.Component {
         const arrayIs = search(name);
         const highScore = this.state.topScore;
         const currentScore = this.state.score;
+        console.log("the current score and highest score", currentScore, highScore);
 
         if (arrayIs.length === 0) {
             newArray.push(parseInt(name));
-            this.setState({
-                clicked: newArray,
-                message: "Good work! Keep going!",
-                alertState: "success"
-            });
 
-            if (highScore < currentScore) {
+            if (highScore <= currentScore && currentScore < 11) {
                 this.setState({
+                    clicked: newArray,
+                    message: "Good work! Keep going!",
+                    alertState: "success",
                     score: this.state.score + 1,
                     topScore: this.state.score + 1
                 })
-            } else if (highScore > currentScore) {
+                this.shuffleAll();
+            } else if (highScore > currentScore && currentScore < 11) {
                 this.setState({
+                    clicked: newArray,
+                    message: "Good work! Keep going!",
+                    alertState: "success",
                     score: this.state.score + 1
                 })
+                this.shuffleAll();
+            } else if (highScore === currentScore && currentScore === 11) {
+                this.setState({
+                    message: "Congratulations! You've won!",
+                    alertState: "success",
+                    score: this.state.score + 1,
+                    topScore: this.state.score + 1
+                })
+                this.shuffleAll();
+            } else if (highScore > currentScore && currentScore === 11) {
+                this.setState({
+                    message: "Congratulations! You've won!",
+                    alertState: "success",
+                    score: this.state.score + 1
+                })
+                this.shuffleAll();
             } else {
                 this.setState({
                     score: this.state.score + 1,
                     topScore: this.state.score + 1
                 })
             };
-            this.shuffleAll();
         } else {
             this.setState({
                 clicked: [],
-                message: "Oops! You've already selected that image.",
+                message: "Oops! You've already selected that image. Try again!",
                 alertState: "danger",
                 score: 0
             });
             this.shuffleAll();
         }
-
-
-
     }
 
     render() {
         return (
             <>
                 <Navbar>
-                    <Navbar.Brand href="/">Moo-ve It</Navbar.Brand>
+                    <Navbar.Brand href="/you-must-click-it/">Mountain Memory</Navbar.Brand>
                     <Navbar.Toggle />
                     <Navbar.Collapse className="justify-content-end">
                         <Navbar.Text>
                             <ButtonGroup aria-label="Basic example">
-                                <Button variant="secondary">Score: {this.state.score}</Button>
-                                <Button variant="secondary">Top Score: {this.state.topScore}</Button>
+                                <Button variant="secondary" >Score: {this.state.score}</Button>
+                                <Button variant="warning">Top Score: {this.state.topScore}</Button>
                             </ButtonGroup>
                         </Navbar.Text>
                     </Navbar.Collapse>
                 </Navbar>
                 <Jumbotron fluid style={jumboStyle}>
                     <Container>
-                        <h1>Moo-ve It</h1>
+                        <h1>Mountain Memory</h1>
                         <p>Select each image once to win!</p>
                     </Container>
                 </Jumbotron>
@@ -185,10 +254,11 @@ class Click extends React.Component {
                 </Container>
                 <Container>
                     <CardColumns style={cardColOverride}>
-                        {this.state.images.map(image => {
+                        {this.state.images.map((image, index) => {
                             return (
                                 <Card className="p-3">
                                     <Card.Img
+                                        key={index}
                                         name={image.name}
                                         variant="top"
                                         src={image.image}
@@ -197,8 +267,7 @@ class Click extends React.Component {
                                     />
                                 </Card>
                             )
-                        })}
-
+                        })};
                     </CardColumns>
                 </Container>
             </>
